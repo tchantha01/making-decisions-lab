148f708a687b712fb0cd6df22d87969d15911151
Toky Chanthavong <tchantha01@gmail.com>
2022-08-17 16:15:49 -0600

    true

--- lab.js
+++ lab.js
@@ -148,18 +148,24 @@ let passingScore = 7;
 
 //CODE HERE
 
-let changeMyMind = false
+// let changeMyMind = true
 
-if(changeMyMind === true){
-  console.log('false')
-}else{
-  console.log('true')
-}
+// if(changeMyMind === true){
+//   console.log('false')
+// }else{
+//   console.log('true')
+// }
 
-////////// PROBLEM 10 //////////
-// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
+// ////////// PROBLEM 10 //////////
+// // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
-//CODE HERE
+// //CODE HERE
+
+// if(changeMyMind !== true){
+//   console.log('false')
+// }else{
+//   console.log('true')
+// }
 
 ////////// ADVANCED PROBLEMS //////////
 
